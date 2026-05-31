681ff767c748afd406d2ea70add28f5236ecc119
Tomas <tomasschool391@gmail.com>
2026-05-31 13:01:29 -0300

    Habilito CORS, apra que el front peuda hacer consultas

--- src/main.ts
+++ src/main.ts
@@ -25,6 +25,18 @@ async function bootstrap() {
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('api', app, document); // La URL será http://localhost:3000/api
 
+  // HABILITO CORS PARA UE EL FRONT PUEDA CONECTARSE CON EL BACKEND
+  app.enableCors({
+    origin: [
+      'http://localhost:5173', // Vite / React
+      'http://localhost:3000', // otro frontend web
+      'http://192.168.0.10:5173', // tu IP local si accedés desde otro dispositivo
+    ],
+    methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE'],
+    allowedHeaders: ['Content-Type', 'Authorization'],
+    credentials: true,
+  });
+
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
