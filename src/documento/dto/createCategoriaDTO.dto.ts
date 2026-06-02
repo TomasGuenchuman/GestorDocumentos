30899a311dc5bf39b638bc55a6b90e85ff04c4af
Tomas <tomasschool391@gmail.com>
2026-06-02 17:54:45 -0300

    agregue nombre en swagger

--- src/documento/dto/createCategoriaDTO.dto.ts
+++ src/documento/dto/createCategoriaDTO.dto.ts
@@ -1,7 +1,12 @@
 import { IsEnum, IsNotEmpty, IsString, Matches } from 'class-validator';
 import { TipoEntidad } from 'src/common/tipoEntidad.enum';
+import { ApiProperty } from '@nestjs/swagger';
 
 export class CreateCategoriaDTO {
+  @ApiProperty({
+    description: 'Titulo de la categoría (Solo letras, números y espacios)',
+    example: 'habilitacion municipal',
+  })
   @IsString({ message: 'El nombre debe ser un texto' })
   @IsNotEmpty({ message: 'El nombre es obligatorio' })
   @Matches(/^[A-Za-zÁÉÍÓÚáéíóúÑñ\s]+$/, {
