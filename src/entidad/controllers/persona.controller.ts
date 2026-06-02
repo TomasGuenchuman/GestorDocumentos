4cdcbaa990fd622c4a3e56ff15d9aba770d9e351
Tomas <tomasschool391@gmail.com>
2026-06-02 14:33:31 -0300

    agregue restricciones al controller

--- src/entidad/controllers/persona.controller.ts
+++ src/entidad/controllers/persona.controller.ts
@@ -7,6 +7,8 @@ import {
   Param,
   Delete,
   ParseIntPipe,
+  UsePipes,
+  ValidationPipe,
 } from '@nestjs/common';
 import { PersonaService } from '../services/persona.service';
 import { createPersonaDTO } from '../dto/createPersonaDTO.dto';
@@ -17,10 +19,12 @@ export class PersonaController {
   constructor(private readonly personaService: PersonaService) {}
 
   @Post()
+  @UsePipes(new ValidationPipe({ whitelist: true, forbidNonWhitelisted: true }))
   crear(@Body() dto: createPersonaDTO) {
     return this.personaService.crearPersona(dto);
   }
   @Patch(':id')
+  @UsePipes(new ValidationPipe({ whitelist: true, forbidNonWhitelisted: true }))
   update(
     @Param('id', ParseIntPipe) id: number,
     @Body() updatePersonaDto: UpdatePersonaDto,
