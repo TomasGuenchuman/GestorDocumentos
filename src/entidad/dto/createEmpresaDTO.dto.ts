c51e6e15776277d3d77801da4601ef9895eb1e77
Tomas <tomasschool391@gmail.com>
2026-06-02 12:05:59 -0300

    añado datos personalizados al swagger

--- src/entidad/dto/createEmpresaDTO.dto.ts
+++ src/entidad/dto/createEmpresaDTO.dto.ts
@@ -1,6 +1,11 @@
 import { IsString, IsNotEmpty, Matches, Length } from 'class-validator';
+import { ApiProperty } from '@nestjs/swagger'; // 1. Importa el decorador
 
 export class createEmpresaDTO {
+  @ApiProperty({
+    description: 'Nombre o Razón Social de la empresa (Solo letras y espacios)',
+    example: 'Empresa de Logistica SRL', // <--- AQUÍ DEFINES EL PLACEHOLDER
+  })
   @IsString({ message: 'La razón social debe ser un texto' })
   @IsNotEmpty({ message: 'La razón social es obligatoria' })
   @Matches(/^[a-zA-ZáéíóúÁÉÍÓÚñÑ\s]+$/, {
@@ -8,6 +13,11 @@ export class createEmpresaDTO {
   })
   razonSocial: string;
 
+
+  @ApiProperty({
+    description: 'Número de CUIT de la empresa (11 dígitos sin guiones)',
+    example: '20304567891', // <--- AQUÍ DEFINES EL PLACEHOLDER
+  })
   @IsString({ message: 'El CUIT debe ser un texto de números' })
   @IsNotEmpty({ message: 'El CUIT es obligatorio' })
   @Length(11, 11, { message: 'El CUIT debe tener exactamente 11 dígitos' })
